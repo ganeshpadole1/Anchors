c66bc34803c3eea0c9ce4e6c8d32bb8ed41e0067
ganeshpadole <ganeshpadole55@gmail.com>
2023-04-17 17:39:43 +0530

    - added left label and top label

--- Anchors/ViewController.swift
+++ Anchors/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        print("Anchors demo")
+        
+        setupViews()
     }
 
-
+    func setupViews() {
+        let upperLeftLabel = makeLabel(withText: "Upper Left Label")
+        
+        view.addSubview(upperLeftLabel)
+        upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        upperLeftLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        
+        
+        let upperRightLabel = makeLabel(withText: "Upper Right Label")
+        view.addSubview(upperRightLabel)
+        upperRightLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        upperRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+    }
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .yellow
+        label.text = text
+        
+        return label
+    }
 }
 
