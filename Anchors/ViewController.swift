a23902a7bc3b4c46de53ef983d5798a771eaadbd
ganeshpadole <ganeshpadole55@gmail.com>
2023-04-17 18:39:36 +0530

    - more constraints

--- Anchors/ViewController.swift
+++ Anchors/ViewController.swift
@@ -19,16 +19,36 @@ class ViewController: UIViewController {
 
     func setupViews() {
         let upperLeftLabel = makeLabel(withText: "Upper Left Label")
-        
+        let upperRightLabel = makeLabel(withText: "Upper Right Label")
+        let bottomLeftLabel = makeLabel(withText: "Bottom Left")
+        let bottomRightButton = makeButton(withText: "Pay Bill")
+        let redView = makeView()
+     
         view.addSubview(upperLeftLabel)
+        view.addSubview(upperRightLabel)
+        view.addSubview(bottomLeftLabel)
+        view.addSubview(bottomRightButton)
+        view.addSubview(redView)
+        
         upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         upperLeftLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         
-        
-        let upperRightLabel = makeLabel(withText: "Upper Right Label")
-        view.addSubview(upperRightLabel)
-        upperRightLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+         upperRightLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         upperRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        bottomLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        bottomLeftLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
+        
+        bottomRightButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        bottomRightButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
+        
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        redView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100).isActive = true
     }
     
     func makeLabel(withText text: String) -> UILabel {
@@ -38,6 +58,23 @@ class ViewController: UIViewController {
         label.text = text
         
         return label
+    }
+    
+    func makeButton(withText text: String) -> UIButton {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(text, for: .normal)
+        
+        return button
+    }
+    
+    func makeView() -> UIView {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        
+        return view
     }
 }
 
